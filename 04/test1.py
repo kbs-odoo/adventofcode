4343e0e1f1911b7d1cf422440e0df1ffd0db4965
kbs-odoo <kbs@odoo.com>
2022-12-05 00:04:29 +0530

    Day 4

--- 04/test1.py
+++ 04/test1.py
@@ -5,10 +5,7 @@ for i in sys.stdin.readlines():
     pair = i.strip("\n").split(",")
     print(pair)
     i, j = pair[0].split("-"), pair[1].split("-")
-    first, second = {k for k in range(int(i[0]), int(i[-1]) + 1)}, {
-        m for m in range(int(j[0]), int(j[-1]) + 1)
-    }
-    print(first, second)
+    first, second = {k for k in range(int(i[0]), int(i[-1]) + 1)}, {m for m in range(int(j[0]), int(j[-1]) + 1)}
     val = second >= first or first >= second
     if val:
         tot += 1
