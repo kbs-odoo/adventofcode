726e1f1153dacfe68cb2d8dcb09dedfb0c7d34d4
kbs-odoo <kbs@odoo.com>
2022-12-05 00:00:04 +0530

    Day 4

---
+++ 04/test1.py
@@ -0,0 +1,16 @@
+import sys
+
+tot = 0
+for i in sys.stdin.readlines():
+    pair = i.strip("\n").split(",")
+    print(pair)
+    i, j = pair[0].split("-"), pair[1].split("-")
+    first, second = {k for k in range(int(i[0]), int(i[-1]) + 1)}, {
+        m for m in range(int(j[0]), int(j[-1]) + 1)
+    }
+    print(first, second)
+    val = second >= first or first >= second
+    if val:
+        tot += 1
+
+print(tot)
---
+++ 04/test2.py
@@ -0,0 +1,21 @@
+import sys
+
+tot = 0
+overlap = 0
+for i in sys.stdin.readlines():
+    pair = i.strip("\n").split(",")
+    print(pair)
+    i, j = pair[0].split("-"), pair[1].split("-")
+    first, second = {k for k in range(int(i[0]), int(i[-1]) + 1)}, {
+        m for m in range(int(j[0]), int(j[-1]) + 1)
+    }
+    print(first, second)
+    val = second >= first or first >= second
+    if val:
+        tot += 1
+    overlap_set = first.intersection(second)
+    print(overlap_set)
+    if overlap_set != set():
+        overlap += 1
+
+print(overlap)
