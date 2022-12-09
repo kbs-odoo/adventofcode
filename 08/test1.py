6022f28eee50677b442e508fcaf8346fe2f87775
kbs-odoo <kbs@odoo.com>
2022-12-09 10:16:10 +0530

    Day 8

---
+++ 08/test1.py
@@ -0,0 +1,48 @@
+import sys
+
+count=0
+tree =0
+map_tree = {}
+map_tree_cols = {}
+for i in sys.stdin.readlines():
+    count+=1
+    print(i)
+    a = i.strip('\n')
+    map_tree[count] = [int(i) for i in a]
+
+count_col = 0
+tot = 0
+for col in map_tree.values():
+    for j in col:
+        count_col+= 1
+        if count_col == count+1:
+            count_col = 1
+        if map_tree_cols.get(count_col, []):
+            map_tree_cols[count_col] = map_tree_cols[count_col]+[j]
+        else:
+            map_tree_cols[count_col] = [j]
+
+tree = count*4 -4
+
+print("ROWS:" ,map_tree)
+
+for i in map_tree.keys():
+    if i==1 or i==5:
+        continue
+    for j in range(1, len(map_tree[i][1:len(map_tree[i])-1])):
+        if max(map_tree[i][j+1:len(map_tree[i])])< map_tree[i][j]:
+            print(map_tree[i-1])
+
+print("COLS", map_tree_cols)
+
+inn_tree = 0
+for i in range(2, count):
+    print(map_tree[i])
+    for j in range(2, count):
+        print("ROW:", i, "COL", j, map_tree[i][:j-1], map_tree[i][j-1], map_tree[i][j:count+1])
+        print("NEXT LOGIC", map_tree_cols[j][:i-1],map_tree[i][j-1], map_tree_cols[j][i:count+1])
+        if map_tree[i][j-1]>max(map_tree[i][:j-1]) or map_tree[i][j-1]>max(map_tree[i][j:count+1]) or map_tree[i][j-1]>max(map_tree_cols[j][:i-1]) or  map_tree[i][j-1] > max(map_tree_cols[j][i:count+1]):
+            inn_tree +=1
+
+print(inn_tree)
+print(inn_tree+tree)
